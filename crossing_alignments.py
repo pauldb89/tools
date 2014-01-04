879de5765a158065b6a3b401774dffb32f0b34d7
Paul Baltescu <pauldb89@gmail.com>
2014-01-04 18:13:35 +0000

    Track progress in crossing_alignments script.

--- crossing_alignments.py
+++ crossing_alignments.py
@@ -6,6 +6,7 @@ def main():
   alignment_file = open(sys.argv[1], "r")
 
   crossing = 0
+  num_lines = 0
   for line in alignment_file:
     alignment = [tuple(map(int, link.split('-'))) for link in line.split()]
 
@@ -13,6 +14,13 @@ def main():
       for other_link in alignment:
         crossing += link[0] > other_link[0] and link[1] < other_link[1]
 
+    num_lines += 1
+    if num_lines % 10000 == 0:
+      sys.stdout.write('.')
+      if num_lines % 1000000 == 0:
+        sys.stdout.write('\n')
+
+  sys.stdout.write('\n')
   print "Number of crossing aligments:", crossing
 
 if __name__ == "__main__":
