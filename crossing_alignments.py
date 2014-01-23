49798521701dc1725f5b33d1eed4a254606b80a0
Paul Baltescu <pauldb89@gmail.com>
2014-01-23 00:36:09 +0000

    Count total number of alignment pairs.

--- crossing_alignments.py
+++ crossing_alignments.py
@@ -6,6 +6,7 @@ def main():
   alignment_file = open(sys.argv[1], "r")
 
   crossing = 0
+  total = 0
   num_lines = 0
   for line in alignment_file:
     alignment = [tuple(map(int, link.split('-'))) for link in line.split()]
@@ -13,6 +14,7 @@ def main():
     for link in alignment:
       for other_link in alignment:
         crossing += link[0] > other_link[0] and link[1] < other_link[1]
+        total += 1
 
     num_lines += 1
     if num_lines % 10000 == 0:
@@ -23,6 +25,7 @@ def main():
 
   sys.stdout.write('\n')
   print "Number of crossing aligments:", crossing
+  print "Total number of alignment link pairs:", total
 
 if __name__ == "__main__":
   main()
